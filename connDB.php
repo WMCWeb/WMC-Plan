2f8e5948c603d54e7538647406b221fdb7c6f9c1
junyoung <37411959+doorumie92@users.noreply.github.com>
2020-12-29 22:53:50 +0900

    connDB
    php 디비 연결

---
+++ connDB.php
@@ -0,0 +1,23 @@
+<?php
+    $conn=mysqli_connect("127.0.0.1","devUser","abcd123$","accountDB");
+    //$db=mysqli_select_db("accountDB",$conn);
+    if($conn){
+        echo "eb연결성공";
+    }
+    else
+    {
+        echo "db연결실패";
+    }
+    //password 받은 값을 해쉬함수태워서 변환후 $hashpass 에 저장
+    $hashpass = 'example';
+    $sql = "INSERT INTO account(userID, password, authKey) VALUES ('test2', '$hashpass' ,'test')";
+     
+    if ($conn->query($sql) === TRUE) {
+        echo "삽입 성공";
+    } else {
+        echo "Error: " . $sql . "
+    " . $conn->error;
+    }
+     
+    $conn->close();
+    ?>
