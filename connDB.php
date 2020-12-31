865f4fc10201bfc25b4663fef3345f88a9ffb67a
kwonchanmi <kwoncm1999@naver.com>
2020-12-31 22:26:08 +0900

    hash 함수 추가

--- connDB.php
+++ connDB.php
@@ -12,15 +12,17 @@
         echo "db연결실패";
     }
     //password 받은 값을 해쉬함수태워서 변환후 $hashpass 에 저장
-    $hashpass = $userPassword;
+    // sha256 해싱 사용
+    $hashpass = hash("sha256",$userPassword);
+
     $sql = "INSERT INTO account(userID, password, authKey) VALUES ('$userId', '$hashpass' ,'$hashpass')";
-     
+
     if ($conn->query($sql) === TRUE) {
         echo "삽입 성공";
     } else {
         echo "Error: " . $sql . "
     " . $conn->error;
     }
-     
+
     $conn->close();
     ?>
